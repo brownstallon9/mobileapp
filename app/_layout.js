0c4465d9f653dbad6f0548f22b09543f9ae49c0f
Abdulhakeem habeebullah <111925404+brownstallon9@users.noreply.github.com>
2024-03-10 18:39:13 +0000

    add

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,13 @@
 import { Stack } from "expo-router";
+// import { useCallback } from "react";
+// import { useFonts } from "expo-font";
+// import * as Splashscreen from 'expo-splash-screen';
+
+
+// Splashscreen.preventAutoHideAsync();
+// const [fontsLoaded] = useFonts({
+//     DMBold: require('../assets/fonts/DMSans-Bold.ttf')
+// })
 
 const Layout = () => {
     return <Stack />
---
+++ hook/useFetch.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+  headers: {
+    'X-RapidAPI-Key': 'be8a197a6fmshd121da7c71f3e37p1cc926jsn0f8270de99b2',
+    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+  },
+  params: {...query},
+};
+const fetchData = async () => {
+    setIsLoading(true);
+
+     try{
+         const response = await axios.request
+         (options);
+
+         setData(response.data.data)
+         setIsLoading(false);
+      } catch (error) {
+         setError(error);
+         alert('There is an error')
+     } finally{
+         setIsLoading(false)
+    }
+}
+
+
+useEffect(() => {
+    fetchData();
+
+}, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return {
+        data, isLoading,  refetch
+    };
+}
+// error,
+
+export default useFetch;
